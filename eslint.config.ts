39e3561b3c892cdd60e93a3e5caacd65055a30ae
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2026-01-28 19:21:45 +0000

    build: configure eslint for typescript projects

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,15 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+
+export default tseslint.config(
+  { ignores: ["dist", "coverage", "node_modules", "ecosystem.config.cjs", "jest.config.cjs"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: [js.configs.recommended], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-]);
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }]
+    }
+  },
+);
