1172ac19b7dbfe4a21b08c17c9270fba44980b24
Raphael Menyawovor <raphaelmenyawovor@gmail.com>
2026-01-31 01:03:30 +0000

    config: disable redis connection during tests

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -9,14 +9,20 @@ const redisClient = createClient({
   url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log('Redis Client Error', err);
+  }
+});
 
 (async () => {
-  try {
-    await redisClient.connect();
-    console.log('Redis Connected');
-  } catch (err) {
-    console.error('Redis Connection Failed', err);
+  if (process.env.NODE_ENV !== 'test') {
+    try {
+      await redisClient.connect();
+      console.log('Redis Connected');
+    } catch (err) {
+      console.error('Redis Connection Failed', err);
+    }
   }
 })();
 
